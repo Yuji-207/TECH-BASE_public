9655abd4edcae85d0b98c3b99b06e92b1b7f3408
Yuji-207 <57951904+Yuij-207@users.noreply.github.com>
2020-08-01 22:35:59 +0900

    画像圧縮機能を実装

--- mission_6/libraries/database.php
+++ mission_6/libraries/database.php
@@ -24,6 +24,24 @@
       return htmlspecialchars($string, ENT_QUOTES|ENT_HTML5, 'UTF-8');
     }
 
-    
+
+    // 画像圧縮
+
+    function compress_img(string $file) {
+
+      $rate = 5;  // 圧縮率
+      $new_width = $width/$rate;  // 圧縮後の横幅
+      $new_height = $height/$rate;  // 圧縮後の横幅
+
+      list($width, $height) = getimagesize($file);  // 画像サイズの取得
+      $image = file_get_contents($file);  // 画像データの読み込み
+
+      $new_image = imagecreatetruecolor($new_width, $new_height);  // 新規画像作成
+
+      imagecopyresampled($new_image, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);  // 元の画像を圧縮
+
+      return file_get_contents($new_image);
+
+    }
   }
 ?>
